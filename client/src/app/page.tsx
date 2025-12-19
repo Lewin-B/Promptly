a482252652757adfdd95f119fda4375d13c41ed7
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-12-19 12:14:14 -0500

    first commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col justify-center text-center">
+        <p className="text-primary py-2 text-6xl font-bold">Promptly</p>
+        <p className="text-primary text-3xl font-semibold">
+          Learn to vibe code better
+        </p>
+      </div>
+    </main>
+  );
+}
